fedaaba87c022dd968b5d267494891d9c4013799
piri_gao <piri.gao@outlook.com>
2024-07-26 02:09:39 +0800

    add task_memory_to_recall, fix empty function bug

--- scripts/run_hpc.sh
+++ scripts/run_hpc.sh
@@ -2,13 +2,13 @@ source ./set_api.sh
 
 cd ..
 python src/main.py \
-        --num_trials 5 \
-        --num_envs 67 \
-        --run_name "hpc_logs_t5e67" \
+        --num_trials 1 \
+        --num_envs 20 \
+        --run_name "hpc_logs_t1e20" \
         --model "gpt-3.5-turbo"  \
         --agent "hpc"   \
         --env "alfworld" \
-        --is_vector
+        # --is_vector
         # --is_resume \
         # --resume_dir logs/hpc_logs_t5e67_0628_050752 \
         # --start_trial_num 4
