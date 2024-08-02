3e1f53630347b4944f6ae19a45341bece379e295
teboonok <450015317@qq.com>
2024-08-02 15:56:53 +0800

    update script

--- scripts/run_hpc_for_webshop.sh
+++ scripts/run_hpc_for_webshop.sh
@@ -3,8 +3,8 @@ source ./set_api.sh
 cd ..
 python src/main.py \
         --num_trials 5 \
-        --num_envs 67 \
-        --run_name "hpc_logs_t5e67" \
+        --num_envs 100 \
+        --run_name "hpc_logs_webshop" \
         --model "gpt-3.5-turbo"  \
         --agent "hpc"   \
         --env "webshop" \
