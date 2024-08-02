f4b91890c0323f13db1c606f38ffa8b2f8a97b10
teboonok <450015317@qq.com>
2024-08-02 15:49:18 +0800

    add openai

---
+++ scripts/set_openai.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 设置 OpenAI API 的相关环境变量
+export OPENAI_API_KEY="sk-2XzRokvHnv6b98o668655511F708485eA21f72B977B3C1F9-1"
+export OPENAI_API_BASE="http://13.213.124.174:3000/v1"
+export OPENAI_API_BASE_URL="http://13.213.124.174:3000/v1"
+export OPENAI_BASE_URL="http://13.213.124.174:3000/v1"
+
+echo "OpenAI environment variables set."
+
